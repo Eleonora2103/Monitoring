05c8b62bd54f2bce35b10d7f432a7af4acf57a8c
Eleonora2103 <62698166+Eleonora2103@users.noreply.github.com>
2020-04-08 17:44:46 +0200

    Update R_code_point_pattern_analysis.r

--- R_code_point_pattern_analysis.r
+++ R_code_point_pattern_analysis.r
@@ -14,6 +14,49 @@ d <- density(covids)
 plot(d)
 points(covids)
 
+setwd("C:/lab/")
+load("point_pattern.RData")
+
+ls()
+
+# covids: point pattern
+# d: density map
+library(spatstat)
+
+plot(d)
+points(covids)
+
+install.packages("rgdal")
+
+library(rgdal)
+
+# let's input vector ines (x0y0, x1y1, x2y2..)
+coastline <- readOGR("ne_10m_coastline.shp")
+
+plot(coastlines, add=T)         # add:adding the lines to the previous image)
+
+# change of the colour, we are making ramp palette   c=array of colours   (100): all the possible colours from yellow to red
+cl <- colorRampPalette(c("yellow","orange","red"))(100)
+plot(d, col=cl)
+points(covids)
+plot(coastlines, add=T)
+
+# Exercise: new colour ramp palette
+clr <- colorRampPalette(c("light green","yellow","orange","violet"))(100)
+plot(d, col=clr, main="densitites of covid-19")
+points(covids)
+plot(coastlines, add=T)
+
+# export as pdf or png("covid_density.png")
+pdf("covid_density.pdf")
+clr <- colorRampPalette(c("light green","yellow","orange","violet"))(100)
+plot(d, col=clr, main="densitites of covid-19")
+points(covids)
+plot(coastlines, add=T)
+dev.off()
+
+
+
 
 
 
