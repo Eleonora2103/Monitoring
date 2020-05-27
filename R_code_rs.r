c3921589bc271fa0e39e2764b154577030da6884
Eleonora2103 <62698166+Eleonora2103@users.noreply.github.com>
2020-05-27 20:11:30 +0200

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -6,11 +6,12 @@ install.packages("RStoolbox")
 
 setwd("C:/lab/")
 
+#raster <- # imports a single layer, yet satellite images are made of more than one layer
 library(raster)
 
-# import imagines
+# import images
 
-p224r63_2011 <- brick("p224r63_2011_masked.grd")
+p224r63_2011 <- brick("p224r63_2011_masked.grd") # brick: import images with several layer at time
 
 plot(p224r63_2011)
 
@@ -24,6 +25,8 @@ cl <- colorRampPalette(c('black','grey','light grey'))(100)    # c= concatenate
 
 # multiframe of different plots       
 # c= series of characters
+
+par(mfrow=c(2,2))
 
 # B1: blue
 clb <- cl <- colorRampPalette(c('dark blue','blue','light blue'))(100)
@@ -60,12 +63,13 @@ plot(p224r63_2011$B4_sre, col=cln)
 # dev.off to close the previous part
 plotRGB(p224r63_2011, r=3, g=2, b= 1, stretch="Lin")         # stretch= stretching the colour to see it better (Lin: linear)
 
-# highlight the vegetation (3 bend at time)
+# highlight the vegetation (3 bend at time) respect to the other parts of the image
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin") 
 
 # NIR on top of the G component of the RGB
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin") 
 
+# Exercise NIR on top of the B component of the RGB 
 plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin") 
 
 #############
