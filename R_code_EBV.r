3d5f9de48de99620e436aca7fa279415a58fc8a3
Eleonora2103 <62698166+Eleonora2103@users.noreply.github.com>
2020-05-27 17:28:11 +0200

    Update R_code_EBV.r

--- R_code_EBV.r
+++ R_code_EBV.r
@@ -27,8 +27,8 @@ library(RSToolbox)
 sntPCA <- rasterPCA(snt)
 sntPCA
 
-summary(sntPCA$model) #info about output of the model
-# 70% of the info
+summary(sntPCA$model) # info about output of the model
+# 70% of the info (component 1)
 plot(sntPCA$map)
 
 #plot RGB
@@ -47,3 +47,53 @@ plot(sd_snt, col=cl)
 par(mfrow=c(1,2))
 plotRGB(snt,4,3,2, stretch="lin")
 plot(sd_snt, col=cl)
+
+
+####### day 2: cladonia example
+# R_code_cladonia_focal.rFile
+
+setwd("C:/lab/")
+library(raster)
+
+# import the images: two functions --> raster (one single band); brick (import several layer at time)
+clad <- brick("cladonia_stellaris_calaita.JPG")
+
+plotRGB(clad, 1, 2, 3, stretch="lin")
+
+window <- matrix(1, nrow = 3, ncol = 3) # window in which we make the calculation
+window
+
+pairs(clad)
+
+library(RStoolbox)
+
+### PCA analysis
+cladpca <- rasterPCA(clad)
+cladpca
+
+summary(cladpca$model)
+# 98% of the info (component 1)
+plotRGB(cladpca$map, 1, 2, 3, stretch="lin")
+
+sd_clad <- focal(cladpca$map$PC1, w=window, fun=sd) # do the calculation of the diversity into a certain band of the image
+
+PC1_agg <- aggregate(cladpca$map$PC1, fact=10) # accelerate the calculation of sd
+sd_clad_agg <- focal(PC1_agg, w=window, fun=sd)
+
+# plot the two different set we made
+par(mfrow=c(1,2))
+cl <- colorRampPalette(c('yellow','violet','black'))(100) 
+plot(sd_clad, col=cl)
+plot(sd_clad_agg, col=cl)
+
+
+par(mfrow=c(1,2))
+cl <- colorRampPalette(c('yellow','violet','black'))(100) #
+plotRGB(clad, 1,2,3, stretch="lin")
+plot(sd_clad, col=cl)
+# plot(sd_clad_agg, col=cl)
+
+
+
+
+ 
