599999e9c5393d00508408d14f18a652c0cc1cfd
Eleonora2103 <62698166+Eleonora2103@users.noreply.github.com>
2020-06-04 18:15:20 +0200

    Update R_code_pca_remote_sensing.r

--- R_code_pca_remote_sensing.r
+++ R_code_pca_remote_sensing.r
@@ -33,28 +33,32 @@ par(mfrow=c(1,2))
 plotRGB(p224r63_1988, r=5, g=4, b=3, stretch="Lin")
 plotRGB(p224r63_2011, r=5, g=4, b=3, stretch="Lin")
 
-names(p224r63_2011)
+# let's see the correlation between band 1 and band 3
+names(p224r63_2011) # names: to see the names of the variables
 # "B1_sre" "B2_sre" "B3_sre" "B4_sre" "B5_sre" "B6_bt"  "B7_sre"
+# sre: spectrum reflectance; bt: bit transfer -> different kind of reflectance methods
 
-plot(p224r63_2011$B1_sre, p224r63_2011$B3_sre)
+# see how much the bands are correlated each other
+plot(p224r63_2011$B1_sre, p224r63_2011$B3_sre) # $: link two variables
+
 
 # PCA
-# decrease the resolution
-
+# decrease the resolution: we have huge amount of pixels
 p224r63_2011_res <- aggregate(p224r63_2011, fact=10)
 
-# library(RStoolbox) is now needed
-p224r63_2011_pca <- rasterPCA(p224r63_2011_res)
+# library(RStoolbox) is now needed (to make PCA)
+p224r63_2011_pca <- rasterPCA(p224r63_2011_res) 
 
-cl <- colorRampPalette(c('dark grey','grey','light grey'))(100) # 
+cl <- colorRampPalette(c('dark grey','grey','light grey'))(100) # let's plot the $map
 plot(p224r63_2011_pca$map, col=cl)
 
-summary(p224r63_2011_pca$model)
+ # how much variation is explained by each component
+summary(p224r63_2011_pca$model) # summary: to see the information clearly
 # PC1 99.83% of the whole variation
 
-pairs(p224r63_2011)
-plotRGB(p224r63_2011_pca$map, r=1, g=2, b=3, stretc="Lin"))
+pairs(p224r63_2011) # let’s see how the different bands are correlated to each other
 
+plotRGB(p224r63_2011_pca$map, r=1, g=2, b=3, stretc="Lin")) # 1=PC1; 2=PC2; 3=PC3 
 
 p224r63_1988_res <- aggregate(p224r63_1988, fact=10)
 p224r63_1988_pca <- rasterPCA(p224r63_1988_res) 
