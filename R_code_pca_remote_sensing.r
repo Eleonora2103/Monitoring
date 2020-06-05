fc61f2cdd6f64800957149437e733603226404af
Eleonora2103 <62698166+Eleonora2103@users.noreply.github.com>
2020-06-05 11:19:14 +0200

    Update R_code_pca_remote_sensing.r

--- R_code_pca_remote_sensing.r
+++ R_code_pca_remote_sensing.r
@@ -60,22 +60,20 @@ pairs(p224r63_2011) # let’s see how the different bands are correlated to each
 
 plotRGB(p224r63_2011_pca$map, r=1, g=2, b=3, stretc="Lin")) # 1=PC1; 2=PC2; 3=PC3 
 
+# Repeat fot he 1988 image
 p224r63_1988_res <- aggregate(p224r63_1988, fact=10)
 p224r63_1988_pca <- rasterPCA(p224r63_1988_res) 
 plot(p224r63_1988_pca$map, col=cl)
 
 summary(p224r63_1988_pca$model)
+#99.56% by PCI
 
- #99.56% by PCI
 pairs(p224r63_1988)
 
-#difference
+# difference between the two pca maps
 difpca <- p224r63_2011_pca$map - p224r63_1988_pca$map
+plot(difpca)
+
+## since the PC1 is carrying 99% of the whole information, we can plot only this to see the difference
 cldif <- colorRampPalette(c('blue','black','yellow'))(100) 
-
 plot(difpca$PC1,col=cldif)
-
-
-
-
-
