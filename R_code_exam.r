4f2b87d5cf157ab864db0b75f34d2a0a05210631
Eleonora2103 <62698166+Eleonora2103@users.noreply.github.com>
2020-07-18 15:08:36 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -1252,6 +1252,7 @@ plot(May, col=cl)
 
 
 setwd("C:/lab/maremmaJune/")
+
 # Images from June, 2020
 rlist <- list.files(pattern="20200622")
 rlist
@@ -1261,9 +1262,11 @@ June <- stack(import)
 cl <- colorRampPalette(c('red','orange','yellow'))(100) 
 plot(June, col=cl)
 
+# Image from July
+
 setwd("C:/lab/maremmaJuly/")
 # Images from July, 2020
-rlist <- list.files(pattern="20200712")
+rlist <- list.files(pattern="20200707")
 rlist
 
 import <- lapply(rlist, raster)
@@ -1293,4 +1296,47 @@ plotRGB(June, r=3, g=2, b=1, stretch="lin")
 # July
 plotRGB(July, r=3, g=2, b=1, stretch="lin")
 
+par(mfrow=c(1,3))
+# adjust the parameters so the axes colors are white. Also turn off tick marks.
+par(col.axis = "white", col.lab = "white")
+# plot
+plotRGB(May,3,2,1,scale= "20000", stretch = "lin", axes = TRUE, main = "23.05.2020")
+# set bounding box to white as well
+box(col = "white")
 
+plotRGB(June,3,2,1,scale= "20000", stretch = "lin", axes =TRUE, main = "22.06.2020")
+box(col = "white")
+plotRGB(July,3,2,1,scale= "20000", stretch = "lin", axes =TRUE, main = "09.07.2020")
+box(col = "white")
+
+
+# NIR on top of the red component
+par(mfrow=c(1,3))
+plotRGB(May, 4,3,2, scale= "20000", stretch = "lin", axes = TRUE, main = "23.05.2020")
+box(col = "white")
+plotRGB(June, 4,3,2, scale= "20000", stretch = "lin", axes =TRUE, main = "22.06.2020")
+box(col = "white")
+plotRGB(July, 4,3,2, scale= "20000", stretch = "lin", axes =TRUE, main = "09.07.2020")
+box(col = "white")
+
+# NDVI calculation: NIR - RED / NIR + RED
+ndviMay <- (May$T32TPN_20200523T100559_B08 - May$T32TPN_20200523T100559_B04)/(May$T32TPN_20200523T100559_B08 + May$T32TPN_20200523T100559_B04)
+ndviMay <- (May$T32TPN_20200523T100559_B08 - May$T32TPN_20200523T100559_B04)
+/ (May$T32TPN_20200523T100559_B08 + May$T32TPN_20200523T100559_B04)
+ndviJune <- (June$T32TPN_20200622T100559_B8A_20m - June$T32TPN_20200622T100559_B04_20m)
+/ (June$T32TPN_20200622T100559_B8A_20m + June$T32TPN_20200622T100559_B04_20m)
+ndviJuly <- (July$T32TPN_20200707T101031_B8A_20m - July$T32TPN_20200707T101031_B04_20m)
+/ (July$T32TPN_20200707T101031_B8A_20m + July$T32TPN_20200707T101031_B04_20m
+
+# Plot NDVI
+   
+clNDVI = colorRampPalette(c("blue", "white", "red"))()
+par(mfrow=c(2,3))
+plot(ndvi02, col = clNDVI, main = "27/02/2020")
+plot(ndvi03, col = clNDVI, main = "18/03/2020")
+plot(ndvi04a, col = clNDVI, main = "07/04/2020")
+plot(ndvi04b, col = clNDVI, main = "25/04/2020")
+plot(ndvi04c, col = clNDVI, main = "25/04/2020")
+plot(ndvi05, col = clNDVI, main = "30/04/2020")
+            
+            
